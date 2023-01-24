2b6d947a9b4a359cc379614fa30e7f7a29cf5ff9
Vadym Volin <vadim.volin1@gmail.com>
2023-01-24 19:49:14 +0200

    m-2: google oauth -started db implementation

---
+++ mevi-server/src/main/kotlin/mevi/com/configs/db/dao/DatabaseFactory.kt
@@ -0,0 +1,22 @@
+package mevi.com.configs.db.dao
+
+import io.ktor.server.config.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.File
+
+object DatabaseFactory {
+    fun init(config: ApplicationConfig) {
+        val driverClassName = config.property("storage.driverClassName").getString()
+        val jdbcURL = config.property("storage.jdbcURL").getString() +
+                (config.propertyOrNull("storage.dbFilePath")?.getString()?.let {
+                    File(it).canonicalFile.absolutePath
+                } ?: "")
+        val database = Database.connect(jdbcURL, driverClassName)
+        transaction(database) {
+            // Statements here
+            SchemaUtils.create(MODEL_NAME)
+        }
+    }
+}
